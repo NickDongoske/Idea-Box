c1bc53e7a49bfa0ec6ea971a57f6666bc93c0c2a
caleb.haizlett@gmail.com <alanturing@example.c>
2019-10-30 18:14:44 -0600

    add mobile dropdown func

--- main.js
+++ main.js
@@ -3,8 +3,11 @@ var titleInput = document.querySelector('#input-title');
 var bodyInput = document.querySelector('#input-body');
 var saveBtn = document.querySelector('.save-btn');
 var cardContainer = document.querySelector('.card-container');
+var inputContainer = document.querySelector('.input-container');
 var form = document.querySelector('form');
 var cardsArray = JSON.parse(localStorage.getItem("cardsArray")) || [];
+var hamburger = document.querySelector('.hamburger');
+var starDiv = document.querySelector('.star-div');
 
 // EventListeners
 window.addEventListener('load', saveDisableToggle);
@@ -12,22 +15,22 @@ saveBtn.addEventListener('click', addIdea);
 bodyInput.addEventListener('keyup', saveDisableToggle);
 titleInput.addEventListener('keyup', saveDisableToggle);
 cardContainer.addEventListener('click', cardEventHandler);
+hamburger.addEventListener('click', openNav);
 
 reinstantiateArray();
 
 // Function to toggle the use of the save button
 function saveDisableToggle() {
   if (titleInput.value &&
-      bodyInput.value) {
+    bodyInput.value) {
     saveBtn.disabled = false;
     saveBtn.style.backgroundColor = '#1f1f3d';
     saveBtn.style.cursor = 'pointer';
-  }
-  else {
+  } else {
     saveBtn.disabled = true;
     saveBtn.style.backgroundColor = '#7a7a7a';
     saveBtn.style.cursor = 'not-allowed';
-    }
+  }
 }
 
 // Function to add a new idea card
@@ -50,7 +53,7 @@ function appendCard(idea) {
 }
 
 function addIdea() {
-  var idea = new Idea( {
+  var idea = new Idea({
     id: Date.now(),
     title: titleInput.value,
     body: bodyInput.value,
@@ -70,7 +73,7 @@ function reinstantiateArray() {
       star: cardsArray[i].star,
     });
     appendCard(idea);
-    if(cardsArray[i].star === true) {
+    if (cardsArray[i].star === true) {
       document.querySelector(`[data-name="${cardsArray[i].id}"]`).firstElementChild.firstElementChild.classList.add("star-icon-active");
     }
   }
@@ -92,11 +95,11 @@ function deleteCard() {
   if (cardsArray[index].id = event.target.closest(".idea-card").dataset.name) {
     cardsArray.splice(index, 1);
     localStorage.setItem('cardsArray', JSON.stringify(cardsArray));
-    }
+  }
 }
 
 // Function to toggle starred
-function toggleFav () {
+function toggleFav() {
   var specificCardTruthy = event.target.closest(".star-icon").classList.toggle("star-icon-active");
   index = findIndex(event);
   cardsArray[index].star = specificCardTruthy;
@@ -108,10 +111,17 @@ function findId(event) {
 }
 
 function findIndex(event) {
-     var id = findId(event);
-     for (var i = 0; i < cardsArray.length; i++) {
-       if (id === cardsArray[i].id) {
-         return parseInt(i);
-       }
-     }
-   }
+  var id = findId(event);
+  for (var i = 0; i < cardsArray.length; i++) {
+    if (id === cardsArray[i].id) {
+      return parseInt(i);
+    }
+  }
+}
+
+function openNav() {
+  hamburger.classList.toggle('toggleX');
+  starDiv.classList.toggle('display-initial');
+  inputContainer.classList.toggle('opacity');
+  cardContainer.classList.toggle('opacity');
+}
